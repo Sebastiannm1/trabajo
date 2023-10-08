0f7238737a253215eaddb339d52f4bf05ef9b112
FacundoAlbano <39500455@terciariourquiza.edu-ar>
2023-10-08 19:51:58 -0300

    Borro carpetas que no usamos

--- TrabajoProgramacion10/classes/Peliculas.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Conexion.php';
-
-class Pelicula extends Conexion {
-    // Métodos y propiedades de la clase Pelicula aquí
-}
-?>
-
--- TrabajoProgramacion10/classes/Usuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Conexion.php';
-
-class Usuario extends Conexion {
-    // Métodos y propiedades de la clase Usuario aquí
-}
-?>
-
