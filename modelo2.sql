4a422bacc42e1b41c1346175b53d16d093a6d385
IsabellaG21 <40189157@terciariourquiza.edu.ar>
2023-10-11 18:10:06 -0300

    correcion modelo de datos

--- modelo2.sql
+++ modelo2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2023 at 11:03 PM
+-- Generation Time: Oct 11, 2023 at 11:07 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
